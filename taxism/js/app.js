4a154edfa5e6ba92c194f688c45a54e9302b1b67
sipundhal <sipundhal5@gmail.com>
2022-01-27 12:42:28 +0530

    Responsive menu

---
+++ taxism/js/app.js
@@ -0,0 +1,102 @@
+
+(function($) { "use strict";
+
+$(function() {
+    var header = $(".start-style");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 10) {
+            header.removeClass('start-style').addClass("scroll-on");
+        } else {
+            header.removeClass("scroll-on").addClass('start-style');
+        }
+    });
+});		
+    
+//Animation
+
+$(document).ready(function() {
+    $('body.hero-anime').removeClass('hero-anime');
+});
+
+//Menu On Hover
+    
+$('body').on('mouseenter mouseleave','.nav-item',function(e){
+        if ($(window).width() > 750) {
+            var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+            setTimeout(function(){
+            _d[_d.is(':hover')?'addClass':'removeClass']('show');
+            },1);
+        }
+});	
+
+//Switch light/dark
+
+$("#switch").on('click', function () {
+    if ($("body").hasClass("dark")) {
+        $("body").removeClass("dark");
+        $("#switch").removeClass("switched");
+    }
+    else {
+        $("body").addClass("dark");
+        $("#switch").addClass("switched");
+    }
+});  
+
+})(jQuery); 
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+
+
+
+
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+  }
+  
+  function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+      parent.classList.remove("focus");
+    }
+  }
+  
+//   inputs.forEach((input) => {
+//     input.addEventListener("focus", focusFunc);
+//     input.addEventListener("blur", blurFunc);
+//   });
